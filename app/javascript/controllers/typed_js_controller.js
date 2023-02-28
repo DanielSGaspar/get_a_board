77fef6ef3258ef9c173f07a468f1d2164dd2f112
Sofia Eriksson <shadyseal@zohomail.eu>
2023-02-28 15:18:32 +0000

    homepage frontend

--- app/javascript/controllers/typed_js_controller.js
+++ app/javascript/controllers/typed_js_controller.js
@@ -4,7 +4,7 @@ import Typed from "typed.js"
 export default class extends Controller {
   connect() {
     new Typed(this.element, {
-      strings: ["Wanna surf?", "Get a board"],
+      strings: ["Never miss a wave", "Get a board"],
       typeSpeed: 50,
       loop: true
     })
