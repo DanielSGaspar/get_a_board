560078ccdb49d56ed784a04efbffaccb0dc339ce
Sofia Eriksson <shadyseal@zohomail.eu>
2023-02-28 17:44:04 +0000

    added design to the simple form

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-@category = ["Surfboard", "Longboard", "BuggieBoard"]
+@category = ["surfboard", "longboard", "bodyboard"]
 
 @user = User.first
 
